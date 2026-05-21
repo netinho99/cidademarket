9ba33dba73b73761d005f6e96a8c556121c34428
ElongioNetoDevjrweb <136861708+netinho99@users.noreply.github.com>
2026-05-20 22:01:06 -0300

    Update Filters.tsx

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,23 +1,17 @@
 interface Props {
   search: string;
   setSearch: (v: string) => void;
-  setCategory: (v: string) => void;
 }
 
 export function Filters({ search, setSearch }: Props) {
   return (
     <div className="filters">
-      <input
-        placeholder="Buscar produto..."
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-
-      <div className="buttons">
-        <button>Todos</button>
-        <button>Tecnologia</button>
-        <button>Dia a Dia</button>
-        <button>Viagem</button>
+      <div className="searchBox">
+        <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Buscar produtos premium..."
+        />
       </div>
     </div>
   );
